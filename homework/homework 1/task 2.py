45cd196fed24c302ed2c50991e603baa13a75d12
edystan <edigeorges50@gmail.com>
2022-09-08 20:43:26 +0300

    first commit

---
+++ homework/homework 1/task 2.py
@@ -0,0 +1,6 @@
+# task 2
+print('I', 'wish', 'for', 'peace', 'in', 'the', 'world', sep=' ', end='')
+
+# task 3
+print('\n\n#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########'
+      '\n\n#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#')
---
+++ homework/homework 2/main.py
@@ -0,0 +1,16 @@
+name_1 = 'Edy'
+day = 'Wednesday'
+
+# task 1
+print(f'Good day {name_1}! {day} is a perfect day to learn some python.')
+
+# task 2
+name_2 = 'Stan'
+print(name_1 + ' ' + name_2)
+
+# task 3
+a = -22
+b = 3.14
+
+print('Sum: ' + str(a + b) + '\nSubtraction: ' + str(a - b) + '\nDivision: ' + str(a/b) + '\nMultiplication: '
+      + str(a*b) + '\nExponent: ' + str(b**a) + '\nModulus: ' + str(abs(a)) + '\nFloor division: ' + str(a//b))
