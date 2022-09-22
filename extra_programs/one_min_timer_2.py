87d94de75e902869a0dcd8a167c88be5137e906a
edystang <edigeorges50@gmail.com>
2022-09-22 21:48:49 +0300

    added iii

---
+++ extra_programs/one_min_timer_2.py
@@ -0,0 +1,6 @@
+# Create simple timer, for 1 minute. Once 1 minute will past - print "It's time!"
+import time
+
+print("This is the start of the program.")
+time.sleep(60)
+print("This prints 60 seconds later.")
