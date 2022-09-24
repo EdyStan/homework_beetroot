5c013c627f880e8a26b09a14ea3359e55a9bf031
edystang <edigeorges50@gmail.com>
2022-09-24 14:09:42 +0300

    recursion

---
+++ classwork/24 sept/recursive_fct.py
@@ -0,0 +1,11 @@
+# Let’s play with recursion. Make a function with argument index: int - call itself with index + 1. Create an if
+# statement, before recursion call, like index < 10, to no get recursion exception.
+
+def recursive_fct(my_int):
+    my_int += 1
+    print(my_int)
+    if my_int < 10:
+        recursive_fct(my_int)
+
+
+recursive_fct(2)
