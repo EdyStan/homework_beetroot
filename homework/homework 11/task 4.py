24f03992b73c5281956d587561ddd0c2bd99e1fd
edystang <edigeorges50@gmail.com>
2022-09-27 22:45:45 +0300

    task4 hw11

--- homework/homework 11/task 4.py
+++ homework/homework 11/task 4.py
@@ -1,12 +1,8 @@
-
-
-
-# I don't understand what to do :(((((((((((((((((((((((((((((((((((((((((((((((((((
-class MainException:
-
-
-
-
-class CustomException(MainException):
+class CustomException(Exception):
     def __init__(self, msg):
         self.msg = msg
+        with open("logs.txt", "w") as file_object:
+            file_object.write(self.msg)
+
+
+raise CustomException('some text\nI love my life sometimes')
