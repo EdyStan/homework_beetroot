1e54b2233309cc0f6e4b1f650330b3da45576c06
edystang <edigeorges50@gmail.com>
2022-10-16 20:53:30 +0300

    t3h15

--- homework/15_property-decorators-class-decorators/task_3_classes.py
+++ homework/15_property-decorators-class-decorators/task_3_classes.py
@@ -1,14 +1,52 @@
 class TypeDecorators:
-    pass
+
+    @staticmethod
+    def to_int(func):
+        def wraps(*args):
+            try:
+                return int(func(*args))
+            except SystemError as e:
+                raise e
+
+        return wraps
+
+    @staticmethod
+    def to_str(func):
+        def wraps(*args):
+            try:
+                return str(func(*args))
+            except SystemError as e:
+                raise e
+
+        return wraps
+
+    @staticmethod
+    def to_bool(func):
+        def wraps(*args):
+            try:
+                return bool(func(*args))
+            except SystemError as e:
+                raise e
+
+        return wraps
+
+    @staticmethod
+    def to_float(func):
+        def wraps(*args):
+            try:
+                return float(func(*args))
+            except SystemError as e:
+                raise e
+
+        return wraps
+
 
 @TypeDecorators.to_int
-def do_nothing(string: str):
-    return string
+def do_nothing(value):
+    return value
 
 
 @TypeDecorators.to_bool
-def do_something(string: str):
-    return string
-
-assert do_nothing('25') == 25
-assert do_something('True') is True
+def do_something(value):
+    value = str(value)
+    return value
--- homework/15_property-decorators-class-decorators/task_3_testing.py
+++ homework/15_property-decorators-class-decorators/task_3_testing.py
@@ -1,4 +1,4 @@
-from task_3_classes import TypeDecorators, do_nothing, do_something
+from task_3_classes import do_nothing, do_something
 
 assert do_nothing('25') == 25
 assert do_something('True') is True
