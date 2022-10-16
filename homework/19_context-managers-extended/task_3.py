92b1e5e99c3dd390e5883f80459227a792798771
edystang <edigeorges50@gmail.com>
2022-10-16 13:48:30 +0300

    hw19

--- homework/19_context-managers-extended/task_3.py
+++ homework/19_context-managers-extended/task_3.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from task_1_class import MyOpen
 
@@ -9,9 +11,13 @@ def replace_file_obj():
 
 @pytest.fixture
 def data_to_check(replace_file_obj):
+    with MyOpen(replace_file_obj, 'w') as io_file:
+        io_file.write('Some text to test...')
+
     with MyOpen(replace_file_obj, 'r') as io_file:
         data = io_file.read(9)
-        return data
+        yield data
+    os.remove("pytest_input.txt")
 
 
 def test_data(data_to_check):
