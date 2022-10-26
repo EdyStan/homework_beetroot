9f2559d69a99831c9ef8355a2e9d1dc705759e4a
edystang <edigeorges50@gmail.com>
2022-10-26 20:34:15 +0300

    Homework 27 oct

--- classwork/22-10-25/password_generator.py
+++ classwork/22-10-25/password_generator.py
@@ -10,41 +10,46 @@ from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 
 
 def password_generator():
-    length = 0
-    while length < 4 or length > 16:
-        length = input('How long do you want the password to be? (4 - 16 characters): ')
-        if length.isdigit():
-            length = int(length)
-        else:
-            length = 0
-
-    password_alphabet = ''
-    sad_face = ':('
-
-    def use_characters(characters, instance_words):
-        while True:
-            # The following command does not work if the input string is
-            # f'Do you want to use {instance_words}?(y/n): '
-            use_lowercase = input('Do you want to use ' + instance_words + '?(y/n): ')
-            if use_lowercase == 'y':
-                nonlocal password_alphabet
-                password_alphabet += characters
-                break
-            elif use_lowercase == 'n':
-                break
+    while True:
+        length = 0
+        while length < 4 or length > 16:
+            length = input('How long do you want the password to be? (4 - 16 characters): ')
+            if length.isdigit():
+                length = int(length)
             else:
-                nonlocal sad_face
-                print('Please type y or n! ' + sad_face)
-                sad_face += '('
+                length = 0
 
-    character_groups_list = [ascii_uppercase, ascii_lowercase, digits, punctuation]
-    instance_words_list = ['uppercase letters', 'lowercase letters', 'digits', 'symbols']
+        password_alphabet = ''
+        sad_face = ':('
 
-    for character_group, varname in zip(character_groups_list, instance_words_list):
-        use_characters(character_group, varname)
+        def use_characters(characters, instance_words):
+            while True:
+                # The following command does not work if the input string is
+                # f'Do you want to use {instance_words}?(y/n): '
+                use_lowercase = input('Do you want to use ' + instance_words + '?(y/n): ')
+                if use_lowercase == 'y':
+                    nonlocal password_alphabet
+                    password_alphabet += characters
+                    break
+                elif use_lowercase == 'n':
+                    break
+                else:
+                    nonlocal sad_face
+                    print('Please type y or n! ' + sad_face)
+                    sad_face += '('
 
-    password = ''.join(choices(password_alphabet, k=length))
-    print(password)
+        character_groups_list = [ascii_uppercase, ascii_lowercase, digits, punctuation]
+        instance_words_list = ['uppercase letters', 'lowercase letters', 'digits', 'symbols']
+
+        for character_group, varname in zip(character_groups_list, instance_words_list):
+            use_characters(character_group, varname)
+        try:
+            password = ''.join(choices(password_alphabet, k=length))
+            repeat = input(f'Your password is: {password}\nDo you want to keep it?(y/n): ')
+            if repeat == 'y':
+                break
+        except IndexError:
+            print('You have no characters to make a password. Try again accepting at least one set of characters!')
 
 
 password_generator()
