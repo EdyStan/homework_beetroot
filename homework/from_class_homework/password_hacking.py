8357ab3816c412eddd82165b010a3e064b9c49cd
edystang <edigeorges50@gmail.com>
2022-11-28 11:42:46 +0200

    hack

--- homework/from_class_homework/password_hacking.py
+++ homework/from_class_homework/password_hacking.py
@@ -15,26 +15,22 @@ def timer(fn):
 
     def inner(*args, **kwargs):
         start_time = perf_counter()
-        to_execute = fn(*args, **kwargs)
+        fn(*args, **kwargs)
         end_time = perf_counter()
-        execution_time = end_time - start_time
-        print('{0} took {1:.8f}s to execute'.format(fn.__name__, execution_time))
-        return to_execute
+        return end_time - start_time
 
     return inner
 
 
 def password_generator():
     while True:
-        length = randrange(4, 8)
-
-        try:
-            password = ''.join(choices(digits, k=length))
-            repeat = input(f'Your password is: {password}\nDo you want to keep it?(y/n): ')
-            if repeat == 'y':
-                return password
-        except IndexError:
-            print('You have no characters to make a password. Try again accepting at least one set of characters!\n')
+        length = randrange(4, 9)
+        password = ''.join(choices(digits, k=length))
+        # repeat = input(f'Your password is: {password}\nDo you want to keep it?(y/n): ')
+        # if repeat == 'y':
+        #     return password
+        print(password + f'\nof length {length}')
+        return password
 
 
 @timer
@@ -67,8 +63,23 @@ def hacking_function(password):
                 return 0
 
 
-i_dont_want_to_be_hacked = password_generator()
+list_of_4 = []
+list_of_5 = []
+list_of_6 = []
+list_of_7 = []
+list_of_8 = []
 
-hacking_function(i_dont_want_to_be_hacked)
+for _ in range(100):
+    i_dont_want_to_be_hacked = password_generator()
+    if len(i_dont_want_to_be_hacked) == 4:
+        list_of_4.append(hacking_function(i_dont_want_to_be_hacked))
+    if len(i_dont_want_to_be_hacked) == 5:
+        list_of_5.append(hacking_function(i_dont_want_to_be_hacked))
+    if len(i_dont_want_to_be_hacked) == 6:
+        list_of_6.append(hacking_function(i_dont_want_to_be_hacked))
+    if len(i_dont_want_to_be_hacked) == 7:
+        list_of_7.append(hacking_function(i_dont_want_to_be_hacked))
+    if len(i_dont_want_to_be_hacked) == 8:
+        list_of_8.append(hacking_function(i_dont_want_to_be_hacked))
 
-
+# there, I was trying to make a graph
