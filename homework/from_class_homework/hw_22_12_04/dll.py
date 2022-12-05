3ca607115e94ce24cab1710ed6cd3a94d95b300d
edystang <edigeorges50@gmail.com>
2022-12-05 13:41:29 +0200

    homework

---
+++ homework/from_class_homework/hw_22_12_04/dll.py
@@ -0,0 +1,58 @@
+class Node:
+
+    def __init__(self, value, next_node=None, prev_node=None):
+        self.prev = prev_node
+        self.value = value
+        self.next = next_node
+
+    def __str__(self):
+        return str(self.value)
+
+
+class LinkedList:
+
+    def __init__(self):
+        self._head = None
+        self._tail = None
+        self._index_count = 0
+        self._number_of_elements = 0
+        self._iter_elem = None  # current element for iteration
+
+    def __iter__(self):
+        self._index_count = 0
+        return self
+
+    def __next__(self):
+        while self._index_count < self._number_of_elements:
+            self._index_count += 1
+            if self._iter_elem is None:
+                self._iter_elem = self._head
+            else:
+                self._iter_elem = self._iter_elem.next
+
+            return self._iter_elem
+
+        self._iter_elem = None
+        raise StopIteration
+
+    def add__tail_node(self, value):
+        self._number_of_elements += 1
+        node = Node(value)
+        if self._head is None:
+            self._head = node
+            self._tail = node
+        else:
+            self._tail.next = node
+            node.prev = self._tail
+            self._tail = node
+
+    def add__head_node(self, value):
+        self._number_of_elements += 1
+        node = Node(value)
+        if self._head is None:
+            self._head = node
+            self._tail = node
+        else:
+            self._head.prev = node
+            node.next = self._head
+            self._head = node
---
+++ homework/from_class_homework/hw_22_12_04/testings.py
@@ -0,0 +1,12 @@
+from dll import LinkedList
+
+llist = LinkedList()
+
+for i in range(200):
+    llist.add__head_node(i)
+
+for j in range(500):
+    llist.add__tail_node(j)
+
+for elem in llist:
+    print(elem)
